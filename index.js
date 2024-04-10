0c97aa563dc3b0e1a880e6be19a39fa4ccbab7ec
amuratakamitamu <keikei12261226@gmail.com>
2024-04-10 10:57:51 -0700

    余分なコメントを削除した

--- index.js
+++ index.js
@@ -2,36 +2,32 @@ const express = require('express');
 const sharp = require('sharp');
 const multer = require('multer');
 const fs = require('fs');
-const upload = multer({ dest: 'uploads/' }); // ファイルを一時的に保存するディレクトリ
+const upload = multer({ dest: 'uploads/' });
 
 const app = express();
 const PORT = 3000;
 
-// 画像を処理するエンドポイント
 app.post('/process-image', upload.single('image'), (req, res) => {
     if (!req.file) {
         return res.status(400).send('No file uploaded.');
     }
 
-    // 画像のリサイズと切り取り
     sharp(req.file.path)
         .resize(824, 824, {
             fit: 'cover',
         })
-        .composite([{ // 角の丸み
+        .composite([{
             input: Buffer.from(
                 `<svg><rect x="0" y="0" width="824" height="824" rx="188" ry="188"/></svg>`
             ),
             blend: 'dest-in'
         }])
-        .toFormat('png') // PNG形式に変換
+        .toFormat('png')
         .toBuffer()
         .then((outputBuffer) => {
-            // 処理された画像をBase64に変換してクライアントに返す
             const base64Image = outputBuffer.toString('base64');
             res.send({ imageData: base64Image });
 
-            // 処理が終わったらファイルを削除
             fs.unlinkSync(req.file.path);
         })
         .catch((err) => {
@@ -40,7 +36,6 @@ app.post('/process-image', upload.single('image'), (req, res) => {
         });
 });
 
-// サーバーを起動
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
